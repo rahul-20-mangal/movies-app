8b213b2cc7f3b86d3d96b97ca9390aaab2efb99b
Rahul Mangal <rahul200826101998@gmail.com>
2021-04-26 01:47:31 +0530

    Create detail view of movie

--- movies_app/movie_urls.py
+++ movies_app/movie_urls.py
@@ -5,4 +5,5 @@ app_name = 'movies'
 
 urlpatterns = [
     path('', views.MovieListView.as_view(), name='list'),
+    path('<slug:slug>/', views.movie_detail, name='detail'),
 ]
--- movies_app/views.py
+++ movies_app/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 from .models import Movie
 
 
 class MovieListView(ListView):
     model = Movie
+
+
+def movie_detail(request, slug):
+    movie = get_object_or_404(Movie, slug=slug)
+
+    context = {
+        'movie': movie
+    }
+    return render(request, 'movies_app/movie_detail.html', context=context)
